4aaadab1a1370907f4de4399d8228e96e3758246
Arslan Faisal <40736518+github4rslan@users.noreply.github.com>
2025-09-04 04:23:24 +0500

    Update task.js

--- routes/task.js
+++ routes/task.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Task = require("../models/task");
+const Task = require("../models/Task");
 const jwt = require("jsonwebtoken");
 
 const router = express.Router();
